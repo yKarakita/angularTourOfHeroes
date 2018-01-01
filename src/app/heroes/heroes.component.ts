3e4ae94577207cb9c25f21e72d9a9f219dac704c
yKarakita <taf.749@gmail.com>
2018-01-01 21:34:38 +0900

    started "Master/Detail Components"

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+  selectedHero: Hero;
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
